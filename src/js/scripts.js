b544eac3b527e5978210c7f61118ec56f05708e5
Alexey Kalayda <aykalaj2@mts.ru>
2023-11-11 06:30:48 +0300

    Удалил portfolio

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -45,10 +45,4 @@ window.addEventListener('DOMContentLoaded', event => {
             }
         });
     });
-
-    // Activate SimpleLightbox plugin for portfolio items
-    new SimpleLightbox({
-        elements: '#portfolio a.portfolio-box'
-    });
-
 });
